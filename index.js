8ae4bc5baf942417f5c729e85115a2e6d7f56a14
vonviz <voncarlo0211@gmail.com>
2024-04-03 17:50:20 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+var typed = new Typed("#multiple-text", {
+  strings: ["Full Stack Developer", "UI UX", "Front End Developer"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
+
+// var typed = new Typed("#multiple-text", {
+//   strings: ["<i>First</i> sentence.", "&amp; a second sentence."],
+//   typeSpeed: 50,
+// });
